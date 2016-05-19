a8b5882d6615b9c17d7764946ada23143185343c
guanda Li <coliguanda@gmail.com>
2016-05-19 00:20:00 -0700

    finished sorting.py.

--- sorting.py
+++ sorting.py
@@ -3,15 +3,23 @@
 num_arr = list()
 print('Enter 10 elements: \n')
 valid_arr =True
-NUM_ARR_LEN = 3
+NUM_ARR_LEN = 10
+num_arr = []
+ans_arr = []
 
 
-def handleMax(list):
-    #cur = list[0]
-    list.pop(1)
-    print('After pop list[1] at handleMax:  ', list)
+def handleMax(num_arr):
+    cur_max = num_arr[0]
+    max_index = 0
+    for i in range (1,len(num_arr) ):
+        if num_arr[i] > cur_max:
+            cur_max = num_arr[i]
+            max_index = i
+
+    num_arr.pop(max_index)
+    print('After pop list[1] at handleMax:  ', num_arr)
     #for i range(0, )
-    return
+    return [cur_max]
 
 
 for i in range(0, NUM_ARR_LEN):
@@ -23,7 +31,13 @@ for i in range(0, NUM_ARR_LEN):
         valid_arr = False
         break;
 if valid_arr:
-            print("Array is :" , num_arr)
-            handleMax(list)
-            print('After deleted list[0] at main:  ', list)
 
+            print("Array is :" , num_arr)
+            length = len(num_arr)
+            for i in range (0,length ):
+                max_val = handleMax(num_arr)
+                #print('cur_max is  :',max )
+                ans_arr.append(max_val)
+                #print('After deleted list[0] at main:  ', num_arr)
+
+            print('sorted arr is :',ans_arr)
